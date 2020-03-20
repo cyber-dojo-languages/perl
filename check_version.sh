579239093ff2fb1e2e6cc3688bc89b4bf0ba16d6
Jon Jagger <jon@jaggersoft.com>
2020-03-20 06:14:51 +0000

    Now at 50.3.2

--- check_version.sh
+++ check_version.sh
@@ -6,7 +6,7 @@ readonly JSON=`cat docker/image_name.json`
 readonly IMAGE_NAME="${BASH_REMATCH[1]}"
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
-readonly EXPECTED=5.30.1
+readonly EXPECTED=5.30.2
 readonly ACTUAL=$(docker run --rm -it ${IMAGE_NAME} sh -c 'perl --version')
 
 if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
